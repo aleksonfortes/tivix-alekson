de446fb1febe0702fb82d2d90b159837face933f
aleksonfortes <44620844+aleksonfortes@users.noreply.github.com>
2022-08-18 10:25:30 -0400

    Update tivix.cy.js

--- cypress/e2e/tivix.cy.js
+++ cypress/e2e/tivix.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 //////
-// ● I created the main structure about cypress for Tivit interview page, we still have many opportunities to improve this small project.
+// ● I created the main structure about cypress for Tivix interview page, we still have many opportunities to improve this small project.
 // ● We could do much more tests, but I think here is enough to show my knowledge about cypress.
 // ● Also, would be nice to work closer to developers to plan the tests and do some scenarios in the componenet/integration level instead of e2e tests.
 // ● I created many TODOs here regarding selectors. What I would suggest is using data-cy element to map better the page elements as cypress docs recommends.
